765db8f36c005d6c8e7389e1661393544e3ef8ee
Dhyey Parmar <dhyeyp189@gmail.com>
2026-07-06 22:09:20 +0530

    Time: 26 ms (76.66%), Space: 107.2 MB (83.32%) - LeetHub

---
+++ 2126-destroying-asteroids/2126-destroying-asteroids.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+        Arrays.sort(asteroids);
+        long curmass=mass;
+        for(int i=0;i<asteroids.length;i++){
+            if(curmass>=asteroids[i]){
+                curmass+=asteroids[i];
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
