9cbf18b24560259bde97e07909d12812b284f0c6
Dhyey Parmar <dhyeyp189@gmail.com>
2026-07-21 22:37:47 +0530

    Time: 1 ms (89.47%), Space: 43.3 MB (27.45%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        int capitalCount=0;
+        for(int i=0;i<word.length();i++){
+            if(Character.isUpperCase(word.charAt(i))) capitalCount++;
+        }
+        return capitalCount==0||capitalCount==word.length()||(capitalCount==1&&Character.isUpperCase(word.charAt(0)));
+    }
+}
