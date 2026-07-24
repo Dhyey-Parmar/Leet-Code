1678e4bc8bd1fc3a590c672e894ead2058ddf8d8
Dhyey Parmar <dhyeyp189@gmail.com>
2026-07-24 22:12:51 +0530

    Time: 2 ms (99.5%), Space: 47.8 MB (16.23%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int[] count = new int[ratings.length];
+
+        for (int i = 0; i < ratings.length; i++) {
+            count[i]=1;
+        }
+
+        for (int i = 1; i < ratings.length; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                count[i] = count[i - 1] + 1;
+            }
+        }
+
+        for (int i = ratings.length - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                count[i] = Math.max(count[i], count[i + 1] + 1);
+            }
+        }
+
+        int sum = 0;
+        for (int i = 0; i < ratings.length; i++) {
+            sum += count[i];
+        }
+
+        return sum;
+    }
+}
